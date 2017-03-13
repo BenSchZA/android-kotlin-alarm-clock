274e18ad214709e146210798a1ee0ecf9895b29d
BenSchZA <bscholtz.bds@gmail.com>
2017-03-13 12:12:30 +0200

    Alarm creation display time until next alarm.

--- app/src/main/java/com/roostermornings/android/fragment/FriendsRequestFragment2.java
+++ app/src/main/java/com/roostermornings/android/fragment/FriendsRequestFragment2.java
@@ -85,11 +85,9 @@ public class FriendsRequestFragment2 extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_friends_fragment1, container, false);
-        ButterKnife.bind(this, view);
 
-        return view;
+        // Inflate the layout for this fragment
+        return initiate(inflater, R.layout.fragment_friends_fragment2, container, false);
     }
 
     //NB: bind ButterKnife to view and then initialise UI elements
