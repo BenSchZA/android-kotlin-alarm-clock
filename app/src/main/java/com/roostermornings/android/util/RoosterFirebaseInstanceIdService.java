d5b9d1c56799cc8d22c0a7cc86fe6d7954abd25c
Steven Ellis <steven.conrad@gmail.com>
2017-03-15 17:18:54 +0200

    Prepare application to generate and receive FCM messages.

---
+++ app/src/main/java/com/roostermornings/android/util/RoosterFirebaseInstanceIdService.java
@@ -0,0 +1,47 @@
+package com.roostermornings.android.util;
+
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class RoosterFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth == null) return;
+
+        FirebaseUser user = auth.getCurrentUser();
+
+        String deviceToken = FirebaseInstanceId.getInstance().getToken();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.getReference(String.format("users/%s/device_type", user.getUid())).setValue("android");
+        database.getReference(String.format("users/%s/device_token", user.getUid())).setValue(deviceToken);
+
+    }
+}
