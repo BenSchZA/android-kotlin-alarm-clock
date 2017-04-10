a5c8f9b41d83836d85f3b6888eca7858671e70d6
BenSchZA <bscholtz.bds@gmail.com>
2017-04-10 12:19:39 +0200

    Added files to Git - drawables.

--- app/src/main/java/com/roostermornings/android/activity/SettingsActivity.java
+++ app/src/main/java/com/roostermornings/android/activity/SettingsActivity.java
@@ -6,11 +6,22 @@
 package com.roostermornings.android.activity;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import com.roostermornings.android.R;
+import com.roostermornings.android.activity.base.BaseActivity;
 import com.roostermornings.android.fragment.SettingsFragment;
 
-public class SettingsActivity extends Activity {
+import butterknife.BindView;
+
+public class SettingsActivity extends BaseActivity {
+
+//    @BindView(R.id.toolbar_title)
+//    TextView toolbarTitle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
