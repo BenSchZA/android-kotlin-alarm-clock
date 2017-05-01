2369f1aac3fd04df226059d6d962631e3c60d3c7
BenSchZA <bscholtz.bds@gmail.com>
2017-05-01 21:37:42 +0200

    Firebase auth state listener and signout methods added to BaseApplication. Auth listener method added for starting services.

--- app/src/main/java/com/roostermornings/android/BaseApplication.java
+++ app/src/main/java/com/roostermornings/android/BaseApplication.java
@@ -5,29 +5,41 @@
 
 package com.roostermornings.android;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.icu.util.Calendar;
+import android.preference.PreferenceManager;
 import android.service.notification.NotificationListenerService;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.stetho.Stetho;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.roostermornings.android.activity.SplashActivity;
 import com.roostermornings.android.dagger.DaggerRoosterApplicationComponent;
 import com.roostermornings.android.dagger.RoosterApplicationComponent;
 import com.roostermornings.android.dagger.RoosterApplicationModule;
 import com.roostermornings.android.node_api.IHTTPClient;
 import com.roostermornings.android.receiver.BackgroundTaskReceiver;
+import com.roostermornings.android.service.FirebaseListenerService;
 import com.roostermornings.android.sqldata.AudioTableHelper;
 import com.roostermornings.android.sqldata.DeviceAlarmTableHelper;
+import com.roostermornings.android.sqlutil.AudioTableManager;
+import com.roostermornings.android.sqlutil.DeviceAlarmController;
 import com.roostermornings.android.util.Constants;
 import com.roostermornings.android.util.FontsOverride;
+
+import javax.inject.Inject;
 
 import io.fabric.sdk.android.Fabric;
 import retrofit.GsonConverterFactory;
@@ -42,6 +54,7 @@ public class BaseApplication extends android.app.Application {
     public Retrofit mRetrofit;
     public IHTTPClient mAPIService;
     protected FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     protected DatabaseReference mDatabase;
 
     public static Context AppContext;
@@ -53,12 +66,17 @@ public class BaseApplication extends android.app.Application {
     private static int roosterCount;
     private static int friendRequests;
 
+    @Inject
+    public SharedPreferences sharedPreferences;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         //Context to be used in all fragments that could be detached
         AppContext = getApplicationContext();
+
+        sharedPreferences = getSharedPreferences(this.getString(R.string.preferences_key), Context.MODE_PRIVATE);
 
         Fabric.with(this, new Crashlytics());
 
@@ -115,13 +133,88 @@ public class BaseApplication extends android.app.Application {
 //            dbAlarm.close();
         }
 
+        //Set default application settings preferences - don't overwrite existing if false
+        setPreferenceManagerDefaultSettings(false);
+
+        // Add Firebase auth state listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    startBackgroundServices();
+                    // Start Firebase listeners applicable to all activities - primarily to update notifications
+                    if(!isServiceRunning(FirebaseListenerService.class))
+                        startService(new Intent(getApplicationContext(), FirebaseListenerService.class));
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    signOut();
+                }
+            }
+        };
+
+        if (mAuth == null) mAuth = FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public void signOut() {
+        try {
+            //Ensure no audio remaining from old user
+            AudioTableManager audioTableManager = new AudioTableManager(this);
+            audioTableManager.removeAllSocialAudioItems();
+            audioTableManager.removeAllChannelAudioFiles();
+            //Ensure no alarms left from old user
+            DeviceAlarmController deviceAlarmController = new DeviceAlarmController(this);
+            deviceAlarmController.deleteAllLocalAlarms();
+            //Cancel background task intents
+            BackgroundTaskReceiver backgroundTaskReceiver = new BackgroundTaskReceiver();
+            backgroundTaskReceiver.scheduleBackgroundCacheFirebaseData(this, false);
+            backgroundTaskReceiver.scheduleBackgroundDailyTask(this, false);
+            backgroundTaskReceiver.scheduleBackgroundUpdateNotificationsTask(this, false);
+            //Set default application settings preferences - don't overwrite existing if false
+            setPreferenceManagerDefaultSettings(true);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        //Go to splash activity and onboarding
+        Intent intent = new Intent(BaseApplication.this, SplashActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void setPreferenceManagerDefaultSettings(Boolean overwrite) {
+        // ensure user settings are set to default once when new user
+        // As long as you set the third argument to false, you can safely call this method
+        // every time your activity starts without overriding the user's saved preferences
+        // by resetting them to the defaults. However, if you set it to true, you will
+        // override any previous values with the defaults.
+
+        if(overwrite) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.remove("pref_key_user_settings").apply();
+        }
+        PreferenceManager.setDefaultValues(this, R.xml.application_user_settings, overwrite);
+    }
+
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void startBackgroundServices() {
         BackgroundTaskReceiver backgroundTaskReceiver = new BackgroundTaskReceiver();
         backgroundTaskReceiver.scheduleBackgroundCacheFirebaseData(getApplicationContext(), true);
         backgroundTaskReceiver.scheduleBackgroundDailyTask(getApplicationContext(), true);
         backgroundTaskReceiver.scheduleBackgroundUpdateNotificationsTask(getApplicationContext(), true);
-        //Start listener service to download Roosters on notification
-        Intent notificationListenerIntent = new Intent(this, NotificationListenerService.class);
-        startService(notificationListenerIntent);
     }
 
     private void updateNotification() {
