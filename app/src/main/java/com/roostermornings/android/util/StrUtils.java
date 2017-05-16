62739bddea493098fbc48ac20cb572ceaa2d6c2f
BenSchZA <bscholtz.bds@gmail.com>
2017-05-16 21:43:55 +0200

    String notEmptyOrNull check added to StrUtils class. This fixes action button string bug in alarm activation screen broadcastreceiver, and can be used more widely: TODO.

---
+++ app/src/main/java/com/roostermornings/android/util/StrUtils.java
@@ -0,0 +1,12 @@
+/*
+ * Rooster Mornings Android.
+ * Copyright (c)  2017 Roosta Media. All rights reserved.
+ */
+
+package com.roostermornings.android.util;
+
+public class StrUtils {
+    public static boolean notNullOrEmpty(String str) {
+        return (str != null && !"".equals(str));
+    }
+}
