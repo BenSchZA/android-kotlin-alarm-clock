cfb01a438687aa1476e11e14a140fab7e70a6fa4
BenSchZA <bscholtz.bds@gmail.com>
2017-12-10 15:16:44 +0200

    v2.3.5: Android M Widget Service Fix
    Added fix from Beta for starting widget service on Android M devices. Crashlytics #174 - IllegalStateException: Not allowed to start service Intent { cmp=com.roostermornings.android/.widgets.WidgetService }: app is in background

---
+++ app/src/main/java/com/roostermornings/android/receiver/WidgetBroadcastReceiver.kt
@@ -0,0 +1,28 @@
+/*
+ * Rooster Mornings Android.
+ * Copyright (c)  2017 Roosta Media. All rights reserved.
+ */
+
+package com.roostermornings.android.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.roostermornings.android.util.RoosterUtils
+import com.roostermornings.android.widgets.AlarmToggleWidget
+import com.roostermornings.android.widgets.WidgetService
+
+class WidgetBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if(RoosterUtils.hasM()) {
+            AlarmToggleWidget.sendUpdateBroadcast(context)
+        } else {
+            when (intent.action) {
+                Intent.ACTION_USER_PRESENT -> context.startService(Intent(context, WidgetService::class.java))
+                Intent.ACTION_DREAMING_STOPPED -> context.startService(Intent(context, WidgetService::class.java))
+                Intent.ACTION_BOOT_COMPLETED -> context.startService(Intent(context, WidgetService::class.java))
+            }
+        }
+    }
+}
